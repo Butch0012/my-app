0aa52a7f98fb173ef4630febb2f4d7e26279181a
Elnathan Mburu <elnathanmburu@gmail.com>
2023-11-09 22:49:50 +0300

    react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+       <div className="App">
+       <p>
+          Welcome to react.
+        </p>
+        
+
+    </div>
+  );
+}
+
+export default App;
